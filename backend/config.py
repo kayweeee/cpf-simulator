62d72a6ff32373a96182a72a004b93cc6d111cd5
Abzzt <saxaphoneviolin@gmail.com>
2024-03-12 15:30:07 +0800

    Added db sessions

---
+++ backend/config.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from pydantic_settings import (BaseSettings, SettingsConfigDict,)
+
+
+
+class DatabaseConfig(BaseModel):
+    """Backend database configuration parameters.
+
+    Attributes:
+        dsn:
+            DSN for target database.
+    """
+
+    dsn: str = "mysql+pymysql://root:test1234!@localhost:3306/cpf_simulator_db1"
+
+
+class Config(BaseSettings):
+    """API configuration parameters.
+
+    Automatically read modifications to the configuration parameters
+    from environment variables and ``.env`` file.
+
+    Attributes:
+        database:
+            Database configuration settings.
+            Instance of :class:`app.backend.config.DatabaseConfig`.
+        token_key:
+            Random secret key used to sign JWT tokens.
+    """
+
+    database: DatabaseConfig = DatabaseConfig()
+    token_key: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="MYAPI_",
+        env_nested_delimiter="__",
+        case_sensitive=False,
+    )
+
+
+config = Config()
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, Depends, status, HTTPException
-from sqlalchemy.orm  import sessionmaker, Session
-from typing import Annotated
-import models.user as user
-from database.database import engine, SessionLocal
+from sqlalchemy.orm  import Session
+from models.user import UserModel
+
+from database.database import engine, SessionLocal, Base
 from pydantic import BaseModel
+from session import create_session
 
 app = FastAPI()
 
-user.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 class UserBase(BaseModel):
     uuid: int
@@ -23,14 +24,14 @@ def get_db():
         db.close()
 
 @app.post("/user/", status_code=status.HTTP_201_CREATED)
-async def create_user(user: UserBase, db: Session = Depends(get_db)):
-    db_user = user.User(**user.dict())
+async def create_user(user: UserBase, db: Session = Depends(create_session)):
+    db_user = UserModel(**user.dict())
     db.add(db_user)
-    db.commit
+    db.commit()
 
 @app.get("/user/{user_id}", status_code=status.HTTP_201_CREATED)
-async def read_user(user_id:int, db: Session = Depends(get_db)):
-    db_user = db.query(user.UserModel).filter(user.UserModel.uuid == user_id).first()
+async def read_user(user_id:int, db: Session = Depends(create_session)):
+    db_user = db.query(UserModel).filter(UserModel.uuid == user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
