d0175855f30f44fe575bc195b81841566c85df97
seaweedfries <102282955+seaweedfries@users.noreply.github.com>
2024-04-01 20:52:02 +0800

    fix: imported components

--- csa-dashboard/src/App.js
+++ csa-dashboard/src/App.js
@@ -4,69 +4,19 @@ import cpf_image from '../public/cpf_image.png'; // Import your CPF image
 import { FaUser } from 'react-icons/fa';
 import './App.css';
 import Link from 'next/link';
+import Topnavbar from './components/Topnavbar.js';
+import Bottomnavbar from './components/Bottomnavbar.js';
 
-// top nav
-class Loginbar extends Component {
-  render() {
-    return (
-      <nav className='top-nav-menu'>
-        <div className='header'>
-          <img src={cpf_image.src} alt="CPF Image" className="cpf-image" width="80" height="80" />
-          <div className='nav-text'>
-            Central Provident<br />Fund Board Simulator
-          </div>
-        </div>
-
-        <div className='login'>
-          <FaUser className="avatar-icon" />
-          Login 
-
-        </div>
-      </nav>
-    );
-  }
-}
-
-//bottom nav
-  class Bottomnavbar extends Component {
-    render(){
-    return (
-      <nav className='bottom-nav-menu'>
-        <div className='nav-left'>
-          <span><b>CPF Board Simulator</b></span>
-        </div>
-
-        <div className='nav-right'>
-
-          <ul>
-          <li style={{ fontWeight: 'bold', paddingBottom: '10px' }}>Schemes</li>
-            <li><Link href='/retirement'>Retirement</Link></li>
-            <li><Link href='/housing'>Housing</Link></li>
-            <li><Link href='/medisave'>Medisave</Link></li>
-          </ul>
-          <ul>
-          <li style={{ fontWeight: 'bold', paddingBottom: '10px' }}>Products</li>
-            <li><Link href='/home'>Home</Link></li>
-            <li><Link href='/schemes'>Schemes</Link></li>
-            <li><Link href='/faq'>FAQ</Link></li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }}
 // Routing
 export const App = () => {
   return (
     <div>
-      <Loginbar />
+      <Topnavbar loginstate={false} />
       <div className="container">
         <div className="middle-section">
-
-
-
           <form>
-            <h1 style = {{marginTop:'-150px'}}>Log In</h1>
-            <p style={{ marginBottom: '5px',fontSize: '13px'}}>Employee ID or Email</p>
+            <h1 style={{ marginTop: '-150px' }}>Log In</h1>
+            <p style={{ marginBottom: '5px', fontSize: '13px' }}>Employee ID or Email</p>
             <div className="icon-input">
 
               <input
--- csa-dashboard/src/components/Topnavbar.js
+++ csa-dashboard/src/components/Topnavbar.js
@@ -4,29 +4,13 @@ import './Topnavbar.css';
 import cpf_image from '../../public/cpf_image.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-// export default function Topnavbar() {
-//   return (
-//     <nav className='top-nav-menu'>
-//       <div className='header'>
-//         <img src={cpf_image.src} alt="CPF Image" className="cpf-image" width="80" height="80" />
-//         <div className='nav-text'>
-//           Central Provident<br />Fund Board Simulator
-//         </div>
-//       </div>
-//       <div className='nav-links'>
-//         <div>
-//         <Link href='/personal'style={{ textDecoration: 'none', color: 'white' }} >My Profile</Link></div>
-//         <div><Link href='/' style={{ textDecoration: 'none', color: 'white' }}>Get Started</Link></div>
-//         <div><Link href='/faq' style={{ textDecoration: 'none', color: 'white' }}>FAQ</Link></div>
-//       </div>
-//       <div className='logout'>
-//       <Link href='/' style={{ textDecoration: 'none', color: 'white' }}>Logout</Link>
-//       </div>
-//     </nav>
-//   );
-// }
+import { FaUser } from 'react-icons/fa';
+
 export default class Topnavbar extends Component {
   render() {
+
+    const { loginstate } = this.props;
+
     return (
       <>
         <nav className='top-nav-menu'>
@@ -36,20 +20,29 @@ export default class Topnavbar extends Component {
               Central Provident<br />Fund Board Simulator
             </div>
           </div>
-          <div className='nav-links'>
-            <div key={'testing'}>
-              <Link href='/personal' style={{ textDecoration: 'none', color: 'white' }} >My Profile</Link>
-            </div>
-            <div><Link href='/' style={{ textDecoration: 'none', color: 'white' }}>Get Started</Link></div>
-            <div><Link href='/faq' style={{ textDecoration: 'none', color: 'white' }}>FAQ</Link></div>
-          </div>
-          <div className='logout'>
-            <Link href='/' >
-            Logout <FontAwesomeIcon icon={faSignOutAlt} />
-            </Link>
-          </div>
+          {loginstate === true ? (
+            <>
+              <div className='nav-links'>
+                <div key={'testing'}>
+                  <Link href='/personal' style={{ textDecoration: 'none', color: 'white' }}>My Profile</Link>
+                </div>
+                <div><Link href='/' style={{ textDecoration: 'none', color: 'white' }}>Get Started</Link></div>
+                <div><Link href='/faq' style={{ textDecoration: 'none', color: 'white' }}>FAQ</Link></div>
+              </div><div className='logout'>
+                <Link href='/'>
+                  Logout <FontAwesomeIcon icon={faSignOutAlt} />
+                </Link>
+              </div>
+            </>
+          )
+            : (
+              <div className='login'>
+                <FaUser className="avatar-icon" />
+                Login
+              </div>
+            )}
         </nav>
       </>
-       )
-      }
-    }
+    )
+  }
+}
