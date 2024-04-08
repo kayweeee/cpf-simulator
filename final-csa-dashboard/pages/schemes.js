15b392f59574a3f06e68f47e57166410f9408c03
kayweeee <tankaywee09@gmail.com>
2024-04-09 01:13:38 +0800

    edit: minor ui changes

--- final-csa-dashboard/pages/schemes.js
+++ final-csa-dashboard/pages/schemes.js
@@ -8,43 +8,37 @@ import housingimage from "../public/housing.png";
 import SchemeCard from "../components/SchemeCard";
 
 export default function Home() {
-    // scheme card example data (to be replaced with api call)
-    const exampleData = [
-      {
-        scheme_name: "Retirement",
-        questions: 20,
-        scheme_img: retirementimage,
-        enabled: true,
-      },
-      {
-        scheme_name: "Medisave",
-        questions: 20,
-        scheme_img: medisaveimage,
-        enabled: false,
-      },
-      {
-        scheme_name: "Housing",
-        questions: 20,
-        scheme_img: housingimage,
-        enabled: false,
-      },
-      
-      
-      
-      
-    ];
-  
-    return (
-        <div className="text-base">
-        <div>
-          {/* Header */}
-          <div className="w-screen h-auto flex flex-row justify-between items-center px-20 pt-10 pb-10 text-black">
-          <div className="font-bold text-3xl">
-            Schemes
-          </div>
-          </div>
+  // scheme card example data (to be replaced with api call)
+  const exampleData = [
+    {
+      scheme_name: "Retirement",
+      questions: 20,
+      scheme_img: retirementimage,
+      enabled: true,
+    },
+    {
+      scheme_name: "Medisave",
+      questions: 20,
+      scheme_img: medisaveimage,
+      enabled: false,
+    },
+    {
+      scheme_name: "Housing",
+      questions: 20,
+      scheme_img: housingimage,
+      enabled: false,
+    },
+  ];
+
+  return (
+    <div className="text-base">
+      <div>
+        {/* Header */}
+        <div className="w-screen h-auto flex flex-row justify-between items-center px-20 py-5 text-black">
+          <div className="font-bold text-3xl">Schemes</div>
+        </div>
         <div className="flex flex-col gap-y-5 min-h-screen">
-          <div className="flex flex-row flex-wrap px-20 justify-between gap-y-7 my-8">
+          <div className="flex flex-row flex-wrap px-20 justify-between gap-y-7 mb-8">
             {exampleData.map((i) => (
               <SchemeCard
                 key={i.scheme_name}
@@ -54,12 +48,10 @@ export default function Home() {
                 scheme_button={true}
                 enabled={i.enabled}
               />
-            )
-            )}
+            ))}
           </div>
         </div>
-        
-        </div>
-        </div>
-    );}
-      
+      </div>
+    </div>
+  );
+}
