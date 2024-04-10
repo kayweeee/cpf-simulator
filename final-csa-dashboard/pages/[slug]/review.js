248968a6664a9ad58c2a246988afdb770b4be6c7
seaweedfries <102282955+seaweedfries@users.noreply.github.com>
2024-04-11 02:58:52 +0800

    resolving merge conflicts

--- final-csa-dashboard/pages/[slug]/review.js
+++ final-csa-dashboard/pages/[slug]/review.js
@@ -20,35 +20,54 @@ export default function ReviewPage() {
         }
     }, [router.isReady, id, questionContent, answerContent]);
 
-    const piedata = [
-        {
-            value: 47,
-            total: 100
-        },
-        {
-            value: 24,
-            total: 100
-        },
-        {
-            value: 59,
-            total: 100
-        },
-        {
-            value: 83,
-            total: 100
-        }
-    ];
+    const attempt = {
+        accuracy_feedback:
+            "The response lacks specific details that could be included to improve accuracy.",
+        accuracy_score: 2,
+        answer:
+            "string is this lorujhkjsfdh hui ghow i think retirement age is closer to 65 than to 70. How about you try it out?",
+        attempt_id: "1089f47a-70ff-4cb0-90d7-d8cd2c927587",
+        date: "2024-04-11T00:45:05",
+        feedback:
+            "The trainee's response can be improved by providing more specific details to increase accuracy and precision in their answer.",
+        precision_feedback:
+            "The answer is quite brief; additional details could enhance the precision of the response.",
+        precision_score: 2,
+        question_id: "80d834f4-ba81-4efe-b74b-c451882825d6",
+        tone_feedback:
+            "The tone is generally respectful and professional, which is good.",
+        tone_score: 3,
+        user_id: "e76803a6-16b6-4e12-a110-e2f21a705f9e",
+    };
 
-    const feedback = "This is some feedback.";
+    const feedbackData = [
+        {
+            label: "Accuracy",
+            value: attempt.accuracy_score,
+            total: 5,
+            feedback: attempt.accuracy_feedback,
+        },
+        {
+            label: "Precision",
+            value: attempt.precision_score,
+            total: 5,
+            feedback: attempt.precision_feedback,
+        },
+        {
+            label: "Tone",
+            value: attempt.tone_score,
+            total: 5,
+            feedback: attempt.tone_feedback,
+        },
+    ];
 
     return (
         <>
-
             <div className='bg-light-green p-4'>
-                <QuestionBar currentidx={id} review={true}/>
+                <QuestionBar currentidx={id} review={true} />
                 <div className='bg-light-gray rounded-md p-6 m-5 '>
                     <div className="p-4 w-auto h-max-content flex justify-between items-center font-bold">
-                        Feedback
+                        <div className="text-2xl">Feedback</div>
                         <button type="button" className='button'>
                             <Download fontSize='medium' />
                             Download
@@ -59,23 +78,23 @@ export default function ReviewPage() {
                         <p>{questionContent}</p>
                     </div>
                     <div className='pl-4 pr-4 mb-4'>
-                        <h3 className='font-bold'>Your Text:</h3>
+                        <h3 className='font-bold'>Your Answer:</h3>
                         <p>{answerContent}</p>
                     </div>
-                    <div className='pl-4 pr-4 mb-4'>
-                        <h3 className='font-bold'>Feedback:</h3>
-                        <p>{feedback}</p>
+                    <h3 className="px-4 py-2 w-auto h-max-content flex justify-between items-center font-bold">
+                        Overall Scores
+                    </h3>
+                    <div className="pb-4 w-full w-max-screen flex justify-between items-center gap-10 px-5">
+                        {feedbackData.map((i) => (
+                            <div className="flex flex-col justify-center w-1/3 h-auto">
+                                <div className="p-4">
+                                    <RadialGraph data={i} label={i.label} />
+                                </div>
+                                <div className="h-24 text-base text-justify">{i.feedback}</div>
+                            </div>
+                        ))}
                     </div>
-                    <h3 className='p-4 w-auto h-max-content flex justify-between items-center font-bold'>Overall Scores</h3>
-                    <div className='px-20 pb-4 w-auto h-max-content flex justify-between items-center'>
-                        <RadialGraph data={piedata[0]} label={"Accuracy"} />
-                        <RadialGraph data={piedata[1]} label={"Level of Detail"} />
-                        <RadialGraph data={piedata[2]} label={"Conciseness"} />
-                        <RadialGraph data={piedata[3]} label={"Tone"} />
-                    </div>
-
                 </div>
-
                 <ActionBar review={true} />
             </div>
 
