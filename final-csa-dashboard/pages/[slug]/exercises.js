38e6a080e57d031d7b64545ea588d5ea22dd67bf
kayweeee <tankaywee09@gmail.com>
2024-04-11 20:51:41 +0800

    add: api integration + route to review

--- final-csa-dashboard/pages/[slug]/exercises.js
+++ final-csa-dashboard/pages/[slug]/exercises.js
@@ -6,18 +6,20 @@ import { useRouter } from "next/router";
 
 export default function Exercises() {
   const router = useRouter();
-  const [name, setName] = useState(null);
+  const [name, setName] = useState("");
   const [allQuestions, setAllQuestions] = useState([]);
 
   useEffect(() => {
     async function getQuestions() {
+      const user_id = 1;
       if (router.isReady) {
-        setName(router.query.slug);
+        const scheme_name = router.query.slug;
         const res = await fetch(
-          `http://127.0.0.1:8000/question/${router.query.slug}`
+          `http://127.0.0.1:8000/table/${user_id}/${scheme_name}`
         );
         const questions = await res.json();
         setAllQuestions(questions);
+        setName(scheme_name.charAt(0).toUpperCase() + scheme_name.slice(1));
         console.log(questions);
       }
     }
@@ -25,8 +27,14 @@ export default function Exercises() {
     getQuestions();
   }, [router.isReady]);
 
-  function handleNav(question_id) {
+  function handleQuestionNav(question_id) {
     router.push(`/${question_id}/question`, undefined, {
+      shallow: true,
+    });
+  }
+
+  function handleReviewNav(review_id) {
+    router.push(`/${review_id}/review`, undefined, {
       shallow: true,
     });
   }
@@ -60,15 +68,26 @@ export default function Exercises() {
       <div className="w-screen flex items-center justify-center p-4">
         <div className="bg-white min-w-full rounded-md p-6">
           <div className="font-bold text-3xl pt-6 pb-10">{name} Scheme</div>
+
           {/* Table */}
-          <table className="w-full table-auto border border-collapse border-slate-200">
+          <table className="w-full table-fixed border border-collapse border-slate-200">
             <thead>
               <tr>
-                <th className={`${tableCenterCellStyle}`}>Status</th>
-                <th className={`${tableCellStyle}`}>Title</th>
-                <th className={`${tableCenterCellStyle}`}>Difficulty</th>
-                <th className={`${tableCenterCellStyle}`}>Scheme</th>
-                <th className={`${tableCenterCellStyle}`}>Remarks</th>
+                <th className={`${tableCenterCellStyle} bg-dark-grey`}>
+                  Status
+                </th>
+                <th className={`${tableCellStyle} w-1/3 bg-dark-grey`}>
+                  Title
+                </th>
+                <th className={`${tableCenterCellStyle} bg-dark-grey`}>
+                  Difficulty
+                </th>
+                <th className={`${tableCenterCellStyle} bg-dark-grey`}>
+                  Scheme
+                </th>
+                <th className={`${tableCenterCellStyle} bg-dark-grey`}>
+                  Review
+                </th>
               </tr>
             </thead>
 
@@ -77,7 +96,6 @@ export default function Exercises() {
                 <tr
                   key={index + 1}
                   className="hover:bg-light-gray hover:cursor-pointer"
-                  onClick={() => handleNav(question.question_id)}
                 >
                   <td
                     className={`${tableCenterCellStyle}`}
@@ -91,9 +109,10 @@ export default function Exercises() {
                       />
                     )}
                   </td>
-                  <td className={`${tableCellStyle}`}>{`${index + 1}. ${
-                    question.title
-                  }`}</td>
+                  <td
+                    className={`${tableCellStyle} hover:underline hover:underline-offset-2`}
+                    onClick={() => handleQuestionNav(question.question_id)}
+                  >{`${index + 1}. ${question.title}`}</td>
                   <td
                     className={`${tableCenterCellStyle} ${getdifficultyColor(
                       question.question_difficulty
@@ -102,9 +121,16 @@ export default function Exercises() {
                     {question.question_difficulty}
                   </td>
                   <td className={`${tableCenterCellStyle}`}>
-                    {question.scheme_name}
+                    {question.scheme_name.scheme_name}
                   </td>
-                  <td className={`${tableCellStyle}`}>{question.remark}</td>
+                  <td
+                    className={`${tableCenterCellStyle} hover:underline hover:underline-offset-2 `}
+                    onClick={() =>
+                      handleReviewNav("bb1c2cbb-5970-488a-b2d3-97e97d9fd790")
+                    }
+                  >
+                    {question.attempt ? "View Here" : null}
+                  </td>
                 </tr>
               ))}
             </tbody>
